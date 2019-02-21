2e60c8777af188aac65dee3731a73dbccda782e6
leo <l.barayshchuk@gmail.com>
2019-02-22 00:10:42 +0600

    dodelAL

--- matdv/exem.py
+++ matdv/exem.py
@@ -5,11 +5,39 @@ alg=0
 algindex=[]
 geom=0
 geomindex=[]
+dvb8=0
+dvb8index=[]
+dvb9=0
+dvb9index=[]
+dvb10=0
+dvb10index=[]
+dvb11=0
+dvb11index=[]
 for i in range(len(l)):
     p1=l[i].find(' ')
     p2=l[i].find(' ', p1+1)
     p3=l[i].find(' ', p2+1)
     p4=l[i].find(' ', p3+1)
+    if int(l[i][p2:p3])==8 and int(l[i][p3:p4])+int(l[i][p4:])>dvb8:
+        dvb8=int(l[i][p3:p4])+int(l[i][p4:])
+        dvb8index=[i]
+    elif int(l[i][p2:p3])==8 and int(l[i][p3:p4])+int(l[i][p4:])==dvb8:
+        dvb8index.append(i)
+    if int(l[i][p2:p3])==9 and int(l[i][p3:p4])+int(l[i][p4:])>dvb9:
+        dvb9=int(l[i][p3:p4])+int(l[i][p4:])
+        dvb9index=[i]   
+    elif int(l[i][p2:p3])==9 and int(l[i][p3:p4])+int(l[i][p4:])==dvb9:
+        dvb9index.append(i)
+    if int(l[i][p2:p3])==10 and int(l[i][p3:p4])+int(l[i][p4:])>dvb10:
+        dvb10=int(l[i][p3:p4])+int(l[i][p4:])
+        dvb10index=[i]   
+    elif int(l[i][p2:p3])==10 and int(l[i][p3:p4])+int(l[i][p4:])==dvb10:
+        dvb10index.append(i)
+    if int(l[i][p2:p3])==11 and int(l[i][p3:p4])+int(l[i][p4:])>dvb11:
+        dvb11=int(l[i][p3:p4])+int(l[i][p4:])
+        dvb11index=[i]   
+    elif int(l[i][p2:p3])==11 and int(l[i][p3:p4])+int(l[i][p4:])==dvb11:
+        dvb11index.append(i)
     if int(l[i][p3:p4])>alg:
         alg=int(l[i][p3:p4])
         algindex=[i]
@@ -20,6 +48,30 @@ for i in range(len(l)):
         geomindex=[i]
     elif int(l[i][p4:])==geom:
         geomindex.append(i)
+for i in range(len(dvb8index)):
+    p1=l[dvb8index[i]].find(' ')
+    p2=l[dvb8index[i]].find(' ', p1+1)
+    p3=l[dvb8index[i]].find(' ', p2+1)
+    p4=l[dvb8index[i]].find(' ', p3+1)
+    print(l[dvb8index[i]][:p3], dvb8)
+for i in range(len(dvb9index)):
+    p1=l[dvb9index[i]].find(' ')
+    p2=l[dvb9index[i]].find(' ', p1+1)
+    p3=l[dvb9index[i]].find(' ', p2+1)
+    p4=l[dvb9index[i]].find(' ', p3+1)
+    print(l[dvb9index[i]][:p3], dvb9)
+for i in range(len(dvb10index)):
+    p1=l[dvb10index[i]].find(' ')
+    p2=l[dvb10index[i]].find(' ', p1+1)
+    p3=l[dvb10index[i]].find(' ', p2+1)
+    p4=l[dvb10index[i]].find(' ', p3+1)
+    print(l[dvb10index[i]][:p3], dvb10)
+for i in range(len(dvb11index)):
+    p1=l[dvb11index[i]].find(' ')
+    p2=l[dvb11index[i]].find(' ', p1+1)
+    p3=l[dvb11index[i]].find(' ', p2+1)
+    p4=l[dvb11index[i]].find(' ', p3+1)
+    print(l[dvb11index[i]][:p3], dvb11)
 for i in range(len(algindex)):
     p1=l[algindex[i]].find(' ')
     p2=l[algindex[i]].find(' ', p1+1)
